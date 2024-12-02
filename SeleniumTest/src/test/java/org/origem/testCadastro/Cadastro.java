f2dc7b49777cc090280727c2f988e4846f846241
fernando <fernando.candido@aluno.ifsp.edu.br>
2024-12-01 22:57:00 -0300

    feat(cadastro): adicionar método para clicar no botão 'Adicionar'

--- SeleniumTest/src/test/java/org/origem/testCadastro/Cadastro.java
+++ SeleniumTest/src/test/java/org/origem/testCadastro/Cadastro.java
@@ -1,11 +1,24 @@
 package org.origem.testCadastro;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Cadastro {
     private WebDriver driver;
 
     public Cadastro(WebDriver driver) {
         this.driver = driver;
+    }
+    public void clickBataoAdicionar() throws InterruptedException {
+        WebElement addLink = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.linkText("+ Adicionar")));
+        addLink.click();
+
+
     }
 }
