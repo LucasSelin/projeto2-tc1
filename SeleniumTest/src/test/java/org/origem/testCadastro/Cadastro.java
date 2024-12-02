78217d7323bd42b2f03ae8d17c07659c201d2ee0
fernando <fernando.candido@aluno.ifsp.edu.br>
2024-12-01 23:01:58 -0300

    feat(cadastro): adicionar método para cadastrar pessoas com informações pessoais e endereço

--- SeleniumTest/src/test/java/org/origem/testCadastro/Cadastro.java
+++ SeleniumTest/src/test/java/org/origem/testCadastro/Cadastro.java
@@ -21,4 +21,29 @@ public class Cadastro {
 
 
     }
+
+    public void cadastrarPessoas() throws InterruptedException {
+        WebElement addLink = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.linkText("+ Adicionar")));
+        addLink.click();
+
+        driver.findElement(By.id("iCpf")).sendKeys("123.123.123-10");
+        driver.findElement(By.id("iNome")).sendKeys("Fernando");
+        driver.findElement(By.id("iRua")).sendKeys("americo");
+        driver.findElement(By.id("iNumero")).sendKeys("90");
+        driver.findElement(By.id("iCep")).sendKeys("13573000");
+        driver.findElement(By.id("iDataNasc")).sendKeys("1988-10-10");
+        driver.findElement(By.id("iProfissao")).sendKeys("Analista");
+
+        WebElement cadastrarButton = driver.findElement(By.id("cadastrarPessoa"));
+        cadastrarButton.click();
+
+        WebElement notificacao = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.notificacao p")));
+
+        if (!notificacao.isDisplayed()) {
+            throw new IllegalStateException("A notificação não foi exibida após o cadastro.");
+        }
+        //Thread.sleep(2000);
+    }
 }
