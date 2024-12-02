715d5de7978de89e74b99cb079a0169f369ca38b
fernando <fernando.candido@aluno.ifsp.edu.br>
2024-12-01 22:52:27 -0300

    feat:adiciona construtor para inicializar o webDriver

--- SeleniumTest/src/test/java/org/origem/testCadastro/Cadastro.java
+++ SeleniumTest/src/test/java/org/origem/testCadastro/Cadastro.java
@@ -1,4 +1,11 @@
 package org.origem.testCadastro;
 
+import org.openqa.selenium.WebDriver;
+
 public class Cadastro {
+    private WebDriver driver;
+
+    public Cadastro(WebDriver driver) {
+        this.driver = driver;
+    }
 }
