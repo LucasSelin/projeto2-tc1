5a4da3f50cb7332d38eb98f56096edf6afd66e4f
Pedro Bonelli <pebonellimecca@gmail.com>
2024-12-02 20:19:46 -0300

    Feat: Teste cadastro de mesmo CPF

--- ElementsFunctionsSuite/AddSameCPFTest.java
+++ ElementsFunctionsSuite/AddSameCPFTest.java
@@ -3,72 +3,63 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
-import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class AddSameCPFTest {
   private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
+
   @Before
   public void setUp() {
     driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
   }
+
   @After
   public void tearDown() {
     driver.quit();
   }
+
   @Test
   public void addSameCPF() {
+
     driver.get("https://devhub.dev.br/");
     driver.manage().window().setSize(new Dimension(1936, 1056));
+
+
     driver.findElement(By.linkText("+ Adicionar")).click();
-    driver.findElement(By.id("iCpf")).click();
-    driver.findElement(By.id("iCpf")).click();
     driver.findElement(By.id("iCpf")).sendKeys("510.664.638-32");
-    driver.findElement(By.id("iNome")).click();
     driver.findElement(By.id("iNome")).sendKeys("pedro");
-    driver.findElement(By.id("iRua")).click();
     driver.findElement(By.id("iRua")).sendKeys("rua1");
-    driver.findElement(By.id("iNumero")).click();
     driver.findElement(By.id("iNumero")).sendKeys("1");
-    driver.findElement(By.id("iCep")).click();
     driver.findElement(By.id("iCep")).sendKeys("13569160");
-    driver.findElement(By.id("iDataNasc")).click();
-    driver.findElement(By.id("iDataNasc")).sendKeys("0002-12-21");
-    driver.findElement(By.id("iDataNasc")).sendKeys("0020-12-21");
-    driver.findElement(By.id("iDataNasc")).sendKeys("0200-12-21");
     driver.findElement(By.id("iDataNasc")).sendKeys("2002-12-21");
-    driver.findElement(By.id("iProfissao")).click();
     driver.findElement(By.id("iProfissao")).sendKeys("seila");
     driver.findElement(By.id("cadastrarPessoa")).click();
-    {
-      WebElement element = driver.findElement(By.id("cadastrarPessoa"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).perform();
-    }
-    {
-      WebElement element = driver.findElement(By.tagName("body"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element, 0, 0).perform();
-    }
+
+
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".success-message"))); // Substitua pelo seletor correto da mensagem de sucesso
+
+
+    driver.findElement(By.linkText("+ Adicionar")).click();
+    driver.findElement(By.id("iCpf")).sendKeys("510.664.638-32"); // CPF já cadastrado
+    driver.findElement(By.id("iNome")).sendKeys("pedro duplicado");
+    driver.findElement(By.id("iRua")).sendKeys("rua2");
+    driver.findElement(By.id("iNumero")).sendKeys("2");
+    driver.findElement(By.id("iCep")).sendKeys("13569161");
+    driver.findElement(By.id("iDataNasc")).sendKeys("2000-01-01");
+    driver.findElement(By.id("iProfissao")).sendKeys("outro");
+    driver.findElement(By.id("cadastrarPessoa")).click();
+
+
+    WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".error-message"))); // Substitua pelo seletor correto da mensagem de erro
+    assertNotNull("A mensagem de erro não foi exibida!", errorMessage);
+    assertEquals("CPF já cadastrado.", errorMessage.getText());
   }
 }
