f9fbd2abce119ae38342a8b66768bc8add017266
Lucas Selin <lucas.selin@aluno.ifsp.edu.br>
2024-12-03 23:44:35 -0300

    test: ajuste de identacao em teste de interface

--- SeleniumTest/src/test/java/org/origem/InitializationSuite/InitialValidationTesteTest.java
+++ SeleniumTest/src/test/java/org/origem/InitializationSuite/InitialValidationTesteTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
@@ -21,37 +22,51 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-public class InitialValidationTesteTest {
+
+public class InitialValidationTesteTest 
+{
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
+
   @Before
-  public void setUp() {
+  public void setUp() 
+  {
     driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
+
   @After
-  public void tearDown() {
+  public void tearDown() 
+  {
     driver.quit();
   }
+
   @Test
-  public void initialValidationTeste() {
+  public void initialValidationTeste() 
+  {
     driver.get("https://devhub.dev.br/");
     driver.manage().window().setSize(new Dimension(1360, 530));
     driver.findElement(By.linkText("Pessoas")).click();
+
     {
       List<WebElement> elements = driver.findElements(By.xpath("//a[contains(text(),\'Pessoas\')]"));
       assert(elements.size() > 0);
     }
+
     driver.findElement(By.cssSelector("header")).click();
+
     {
       List<WebElement> elements = driver.findElements(By.xpath("//a[contains(text(),\'Sair\')]"));
       assert(elements.size() > 0);
     }
+
     driver.findElement(By.linkText("Sair")).click();
+
     assertThat(driver.getTitle(), is("Pessoas"));
     assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("DevHub"));
+    
     {
       List<WebElement> elements = driver.findElements(By.cssSelector("article"));
       assert(elements.size() > 0);
